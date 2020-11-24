21940b12a242d5bab76dcd90051aff69f2b9aeef
kuntal-ghosh <kuntalghosh.ewu@gmail.com>
2020-11-24 16:43:49 +0600

    dtudent form was added

--- imports/ui/User.vue
+++ imports/ui/User.vue
@@ -1,7 +1,18 @@
 <template>
   <div className="container">
     <b-form @submit="onSubmit" @reset="onReset" v-if="show">
-      <b-form-group
+     
+
+      <b-form-group id="name" label="Your Name:" label-for="name">
+        <b-form-input
+          id="name"
+          v-model="form.name"
+          required
+          placeholder="Enter name"
+        ></b-form-input>
+      </b-form-group>
+
+ <b-form-group
         id="input-group-1"
         label="Email address:"
         label-for="input-1"
@@ -10,22 +21,29 @@
         <b-form-input
           id="input-1"
           v-model="form.email"
+          v-on:input="onChange"
           type="email"
           required
           placeholder="Enter email"
         ></b-form-input>
       </b-form-group>
 
-      <b-form-group id="input-group-2" label="Your Name:" label-for="input-2">
+          <b-form-group id="phone" label="Phone:" label-for="phone">
         <b-form-input
-          id="input-2"
-          v-model="form.name"
+          id="phone"
+          v-model="form.phone"
+          type="number"
           required
-          placeholder="Enter name"
+          placeholder="Enter Phone"
         ></b-form-input>
       </b-form-group>
 
-      <b-form-group id="input-group-3" label="Food:" label-for="input-3">
+    <div class="mb-4">
+    <label for="example-datepicker">Choose a date</label>
+    <b-form-datepicker id="example-datepicker" v-model="form.date" class="mb-2"></b-form-datepicker>
+  </div>
+
+      <!-- <b-form-group id="input-group-3" label="Food:" label-for="input-3">
         <b-form-select
           id="input-3"
           v-model="form.food"
@@ -39,7 +57,7 @@
           <b-form-checkbox value="me">Check me out</b-form-checkbox>
           <b-form-checkbox value="that">Check that out</b-form-checkbox>
         </b-form-checkbox-group>
-      </b-form-group>
+      </b-form-group> -->
 
       <b-button type="submit" variant="primary">Submit</b-button>
       <b-button type="reset" variant="danger">Reset</b-button>
@@ -58,6 +76,8 @@
         form: {
           email: '',
           name: '',
+          phone:'',
+          date: '',
           food: null,
           checked: []
         },
@@ -82,6 +102,10 @@
         this.$nextTick(() => {
           this.show = true
         })
+      },
+      onChange: function()
+      { 
+          console.log("hello");
       }
     }
   }
