de6b140a9a9bccfe221c4d7b6314e7ca461bd9d5
kuntal-ghosh <kuntalghosh.ewu@gmail.com>
2020-11-24 16:42:51 +0600

    routes were added in main.js

--- client/main.js
+++ client/main.js
@@ -1,10 +1,11 @@
 import { Meteor } from 'meteor/meteor';
 import Vue from 'vue';
-
 import App from '../imports/ui/App.vue';
+import router from './router.js';
  
 Meteor.startup(() => {
   new Vue({
+    router,
     el: '#app',
     ...App,
   });
