dc6c71f2816cdeed65d28426564cf3a01be3d262
kuntal-ghosh <kuntalghosh.ewu@gmail.com>
2020-11-25 22:08:31 +0600

    Homepage was updated

--- imports/ui/pages/HomePage.vue
+++ imports/ui/pages/HomePage.vue
@@ -1,12 +1,27 @@
 <template>
-    <div>
-        hello
+    <div style="text-align: center;">
+        <h1>Welcome to YooLibrary</h1>
+        
     </div>
 </template>
 
 <script>
+import {Students} from "../../api/students"
 export default {
-    
+     data() {
+    return {};
+  },
+    meteor:{
+        $subscribe: {
+      // Subscribes to the 'threads' publication with no parameters
+    //   students: []
+    },
+        students()
+        {
+            console.log("called");
+            return Students.find({}).fetch();
+        }
+    }
 }
 </script>
 
