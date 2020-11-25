9bcf5651d768ae32bf04586d102f1a176c61e55d
kuntal-ghosh <kuntalghosh.ewu@gmail.com>
2020-11-25 22:09:08 +0600

    Homepage was updated

--- client/main.js
+++ client/main.js
@@ -1,8 +1,10 @@
 import { Meteor } from 'meteor/meteor';
 import Vue from 'vue';
+import VueMeteorTracker from 'vue-meteor-tracker';
 import App from '../imports/ui/App.vue';
 import router from './router.js';
- 
+Vue.use(VueMeteorTracker);
+
 Meteor.startup(() => {
   new Vue({
     router,
