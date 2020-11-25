04f6ce494e71700fd8f53706a55b1ed112aa2a36
kuntal-ghosh <kuntalghosh.ewu@gmail.com>
2020-11-25 22:07:39 +0600

    nav bar was updated

--- imports/ui/components/Navbar.vue
+++ imports/ui/components/Navbar.vue
@@ -1,7 +1,11 @@
 <template>
     <div>
   <b-navbar toggleable="lg" type="dark" variant="dark" fixed="top">
-    <b-navbar-brand href="#">YooLibrary</b-navbar-brand>
+    
+    <b-navbar-brand href="#">
+    <router-link :to="{ name: 'home' }" exact >YooLibrary</router-link>
+
+      </b-navbar-brand>
 
     <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
@@ -9,19 +13,27 @@
       
       <!-- Right aligned nav items -->
       <b-navbar-nav class="ml-auto">
-        
-        <b-nav-item href="#">Student List</b-nav-item>
-        <b-nav-item href="#">Add Student</b-nav-item>
-        <b-nav-item href="#">Add Subject</b-nav-item>
+        <b-nav-item href="#">
+        <router-link :to="{ name: 'list' }" exact >Student List</router-link>
+        </b-nav-item>
+        <b-nav-item href="#">
+        <router-link :to="{ name: 'user' }" exact >Add Student</router-link>
+        </b-nav-item>
+        <b-nav-item href="#">
+        <router-link :to="{ name: 'subject' }" exact >Add Subject</router-link>
+        </b-nav-item>
+        <b-nav-item href="#">
+                <router-link :to="{ name: 'subjectList' }" exact >Subject List</router-link>
+        </b-nav-item>
 
-        <b-nav-item-dropdown right>
-          <!-- Using 'button-content' slot -->
+
+        <!-- <b-nav-item-dropdown right>
           <template #button-content>
             <em>User</em>
           </template>
           <b-dropdown-item href="#">Profile</b-dropdown-item>
           <b-dropdown-item href="#">Sign Out</b-dropdown-item>
-        </b-nav-item-dropdown>
+        </b-nav-item-dropdown> -->
       </b-navbar-nav>
     </b-collapse>
   </b-navbar>
