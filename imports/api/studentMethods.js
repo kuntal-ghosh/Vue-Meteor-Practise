ebc905ae826876d663bdbf241f40e75281b7f5e7
kuntal-ghosh <kuntalghosh.ewu@gmail.com>
2020-11-25 22:15:35 +0600

    students method were added

---
+++ imports/api/studentMethods.js
@@ -0,0 +1,42 @@
+Meteor.methods({
+    'students.insert'(text) {
+      check(text, String);
+   
+      if (!this.userId) {
+        throw new Meteor.Error('Not authorized.');
+      }
+   
+      TasksCollection.insert({
+        text,
+        createdAt: new Date,
+        userId: this.userId,
+      })
+    },
+   
+    'tasks.remove'(taskId) {
+      check(taskId, String);
+   
+      if (!this.userId) {
+        throw new Meteor.Error('Not authorized.');
+      }
+   
+      TasksCollection.remove(taskId);
+    },
+   
+    'tasks.setIsChecked'(taskId, isChecked) {
+      check(taskId, String);
+      check(isChecked, Boolean);
+   
+      if (!this.userId) {
+        throw new Meteor.Error('Not authorized.');
+      }
+   
+      TasksCollection.update(taskId, {
+        $set: {
+          isChecked
+        }
+      });
+    }
+  });
+
+
